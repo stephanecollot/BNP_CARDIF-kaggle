c16100a66127ee4533a723152c280c8b9e3bdbd8
Stephane Collot <scollot@localhost.localdomain>
2016-04-08 19:01:02 +0200

    Leave one out

--- imputerproba.py
+++ imputerproba.py
@@ -39,16 +39,27 @@ class ImputerProba():
 
     """
 
-    def __init__(self, columnsEncod, columnsProba, verbose=0, copy=True):
+    def __init__(self, columnsEncod, columnsProba, verbose=0, copy=True, leaveOneOut=False):
         self.columnsEncod = columnsEncod
         self.columnsProba = columnsProba
         self.verbose = verbose
         self.copy = copy
+        self.leaveOneOut = leaveOneOut
+        
+    def fit_transform(self, X, y):
+        if self.verbose: print "fit_transform, lengh X: " + str(len(X))
+        
+        self.fit(X, y)
+        return self.transform(X, y=y, training=True)
+        
+        #return self
 
     def fit(self, X, y):
         if self.verbose: print "Fit, lengh X: " + str(len(X))
         self.catEncoders = dict()
         self.catProbability = dict()
+        self.catProbaSum = dict()
+        self.catProbaCount = dict()
 
         # Create the mapping from categorie to int
         for col in self.columnsEncod:
@@ -75,19 +86,27 @@ class ImputerProba():
             if col not in X.columns:
                 if self.verbose: print "Column proba: " + str(col) + " is not in X, skip."
                 continue
+                
             # Cat probability
-            df = pd.pivot_table(_X, values='target', columns=[col], aggfunc=np.average, fill_value=-1) # index=['A', 'B']
-            dictProba = df.to_dict()
-            #dictProba = defaultdict(lambda: -1, dictProba)
-            #print dictProba
-        
-            self.catProbability[col] = dictProba
+            df = pd.pivot_table(_X, values='target', columns=[col], aggfunc=np.average, fill_value=-1) # fillvalue useless
+            probaDict = df.to_dict()
+            if -1 in probaDict: # Removing -1: if we have a missing value then do not replace it with a proba
+                del probaDict[-1]
+            self.catProbability[col] = df.to_dict()
+            
+            # leave-one-out extra calculation
+            if self.leaveOneOut:
+                df = pd.pivot_table(_X, values='target', columns=[col], aggfunc=np.sum, fill_value=-1)
+                self.catProbaSum[col] = df.to_dict()
+                df = pd.pivot_table(_X, values='target', columns=[col], aggfunc=len, fill_value=-1)
+                self.catProbaCount[col] = df.to_dict()
+              
 
         return self
 
 
-    def transform(self, X):
-        if self.verbose: print "Transform"
+    def transform(self, X, y=None, training=False):
+        if self.verbose: print "Transform, training: " + str(training) + "  leaveOneOut: " + str(self.leaveOneOut)
         for col in self.columnsEncod :
             if col not in X.columns:
                 if self.verbose: print "Column encod: " + str(col) + " is not in X, skip."
@@ -112,10 +131,38 @@ class ImputerProba():
         
         if len(self.catEncoders):
             X = X.replace(self.catEncoders)
-        if len(self.catProbability):
-            X = X.replace(self.catProbability)
-
+        
+        if self.leaveOneOut:
+            if training:
+                _X = X.copy() # Very weird bug? Copying is necessary.
+                _X['target'] = y
+                _X = _X.apply(self.rowAction, axis=1)
+                _X = _X.drop('target', axis=1, inplace=False)
+                X = _X
+            else:
+                if len(self.catProbability):
+                    X = X.replace(self.catProbability)
+        else:
+            if len(self.catProbability):
+                X = X.replace(self.catProbability)
+        
+        print X.columns
         return X
+        
+    def rowAction(self, row):
+        for col in self.catProbaSum:
+            caV = row[col] # Category value
+            if caV == -1:
+                continue
+            taV = row['target'] # Target Value
+            sig = self.catProbaSum[col][caV] # Sum of target value in this Cat
+            nbE = self.catProbaCount[col][caV] # Nbr of elements in this Cat
+            if nbE == 1:
+                row[col] = -2 # if one element == to we don't know
+            
+            row[col] = (sig-taV)/(nbE-1.0)
+        
+        return row
 
     def get_params(self, deep=False):
         return {'columnsEncod': self.columnsEncod, 'columnsProba': self.columnsProba}
